82904cce5c710793e4fcd6cf1ac5027812bab8e7
darwinrc <darwinrc@gmail.com>
2020-01-09 22:15:45 -0500

    Updates layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,15 +12,9 @@
   </div>
   <div v-if="isMenuVisible" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Noticias
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Tienda
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-        Historia
-      </a>
+      <g-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/news/">NOTICIAS</g-link>
+      <g-link class=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/shop/">TIENDA</g-link>
+      <g-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white" to="/stories/">HISTORIAS</g-link>
     </div>
     <div>
       <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Donaciones</a>
