04209a9616f74c59186cac5675bcc0c82ce05905
darwinrc <darwinrc@gmail.com>
2020-01-11 12:36:31 -0500

    Adds mobile pills

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,43 +1,55 @@
 <template>
   <div class="layout">
-
     <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6 md:hidden">
       <div class="flex items-center flex-shrink-0 text-white mr-5">
         <img class="logo-responsive" alt="FOCAN Logo" src="../assets/logo-home.png" />
-  </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" @click="toggleMenu">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-    </button>
-  </div>
-  <div v-if="isMenuVisible" class="flex w-full justify-between">
-    <div class="text-sm">
-      <g-link class="block mt-4 text-teal-200 hover:text-white mr-4 text-4xl" to="/news/">NOTICIAS</g-link>
-      <g-link class=" block mt-4 text-teal-200 hover:text-white mr-4 text-4xl" to="/shop/">TIENDA</g-link>
-      <g-link class="block mt-4 text-teal-200 hover:text-white text-4xl" to="/stories/">HISTORIAS</g-link>
-    </div>
-    <div>
-    <div>
-      <a href="#" class="inline-block text-4xl px-5 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Donaciones</a>
-    </div>
-    <div>
-      <a href="#" class="inline-block text-4xl px-8 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Urgencias</a>
-    </div>
-    </div>
-  </div>
-</nav>
-<!--   
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    -->
-    <slot/>
+      </div>
+      <div class="block lg:hidden">
+        <button
+          class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          @click="toggleMenu"
+        >
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div v-if="isMenuVisible" class="flex w-full justify-between">
+        <div class="text-sm">
+          <g-link
+            class="block mt-4 text-teal-200 hover:text-white mr-4 text-4xl"
+            to="/news/"
+          >NOTICIAS</g-link>
+          <g-link class="block mt-4 text-teal-200 hover:text-white mr-4 text-4xl" to="/shop/">TIENDA</g-link>
+          <g-link
+            class="block mt-4 text-teal-200 hover:text-white text-4xl"
+            to="/stories/"
+          >HISTORIAS</g-link>
+        </div>
+        <nav class="navTabs">
+          <div class="navTabs__container">
+            <div class="navTabs__tab">
+              <span class="navTabs__icon"></span>
+              <div class="navTabs__text">
+                <span class="navTabs__text--white">DONAC</span>
+                <span class="navTabs__text--red">IONES</span>
+              </div>
+            </div>
+          </div>
+          <div class="navTabs__container">
+            <div class="navTabs__tab">
+              <span class="navTabs__icon"></span>
+              <div class="navTabs__text">
+                <span class="navTabs__text--white">URGE</span>
+                <span class="navTabs__text--red">NCIAS</span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </nav>
+    <slot />
   </div>
 </template>
 
@@ -51,45 +63,89 @@ query {
 
 <script>
 export default {
-   data:  () => {
+  data: () => {
     return {
-      isMenuVisible: false  
-    }
+      isMenuVisible: false
+    };
   },
-  methods:{
-    toggleMenu () {
-      this.isMenuVisible = !this.isMenuVisible
+  methods: {
+    toggleMenu() {
+      this.isMenuVisible = !this.isMenuVisible;
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
-*{
+* {
   font-size: 10px;
 }
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
 a {
   text-decoration: none;
-  color: #00B8D4;
+  color: #00b8d4;
 
   &:hover {
-    color: #B2EBF2;
+    color: #b2ebf2;
   }
-  
+
   &:active {
-    color: #B2EBF2;
+    color: #b2ebf2;
   }
 }
 
 .logo-responsive {
-  width:100px;
+  width: 100px;
   height: auto;
+}
+.navTabs {
+  margin-top: 2rem;
+  width: 21rem;
+
+  &__tab {
+    position: relative;
+    border: 1px solid red;
+    border-radius: 5rem;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem auto;
+  }
+
+  &__icon {
+    background-color: red;
+    border-radius: 5rem 0 0 5rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    z-index: -1;
+  }
+
+  &__text {
+    color: -webkit-linear-gradient(white, red);
+
+    text-align: center;
+    span {
+      font-size: 2.25rem;
+      font-weight: 400;
+    }
+    &--white {
+      color: white;
+    }
+
+    &--red {
+      color: red;
+    }
+  }
 }
 </style>
