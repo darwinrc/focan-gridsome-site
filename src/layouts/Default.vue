0f536aa37f0e046e0b5e589f1a21e2ddf810f030
darwinrc <darwinrc@gmail.com>
2020-01-11 13:40:07 -0500

    Updates header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="block lg:hidden">
         <button
-          class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          class="flex items-center px-3 py-2 border rounded"
           @click="toggleMenu"
         >
           <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
